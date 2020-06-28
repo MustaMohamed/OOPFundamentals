2970b2fa30d274ca841f73022365f8ba559bdcc2
Musta Mohamed <mustafa.mohamed.wd@gmail.com>
2020-06-28 13:44:00 +0200

    refactor SRP

--- OOPFundamentals/OOPFundamentals/SolidPrinciples/SingleResponsibility/Solution/Program.cs
+++ OOPFundamentals/OOPFundamentals/SolidPrinciples/SingleResponsibility/Solution/Program.cs
@@ -1,40 +1,39 @@
-﻿using System;
-using Solution.Core;
+﻿using Solution.Core;
 
 namespace Solution
 {
-    class Program
+    static class Program
     {
+        private static readonly ILogger Logger = new Logger();
+        private static readonly IApplicationStarter ApplicationStarter = new ApplicationStarter(Logger);
+        private static readonly IApplicationFinisher ApplicationFinisher = new ApplicationFinisher(Logger);
+        private static readonly IPersonValidator PersonValidator = new PersonValidator(Logger);
+        private static readonly IPersonCreator PersonCreator = new PersonCreator(Logger);
+        private static readonly StandardMessages StandardMessages = new StandardMessages(ApplicationStarter, ApplicationFinisher, Logger);
+        private static readonly PersonDataCapture PersonDataCapture = new PersonDataCapture();
+
         static void Main(string[] args)
         {
-            ILogger logger = new Logger();
-            IApplicationStarter applicationStarter = new ApplicationStarter(logger);
-            IApplicationFinisher applicationFinisher = new ApplicationFinisher(logger);
-            IPersonValidator personValidator = new PersonValidator(logger);
-            IPersonCreator personCreator = new PersonCreator(logger);
-            StandardMessages standardMessages = new StandardMessages(applicationStarter, applicationFinisher, logger);
-            PersonDataCapture personDataCapture = new PersonDataCapture();
-
-            standardMessages.StartApplication();
+            StandardMessages.StartApplication();
 
             Person person = new Person();
 
-            standardMessages.DisplayFirstNameRequestMessage();
-            person = personDataCapture.CapturePersonFirstName(person);
+            StandardMessages.DisplayFirstNameRequestMessage();
+            person = PersonDataCapture.CapturePersonFirstName(person);
 
-            standardMessages.DisplayLastNameRequestMessage();
-            person = personDataCapture.CapturePersonLastName(person);
+            StandardMessages.DisplayLastNameRequestMessage();
+            person = PersonDataCapture.CapturePersonLastName(person);
 
-            bool isValidPerson = personValidator.Validate(person);
+            bool isValidPerson = PersonValidator.Validate(person);
 
             if (!isValidPerson)
             {
-                applicationFinisher.FinishApplication();
+                ApplicationFinisher.FinishApplication();
                 return;
             }
 
-            personCreator.Create(person);
-            applicationFinisher.FinishApplication();
+            PersonCreator.Create(person);
+            ApplicationFinisher.FinishApplication();
         }
     }
 }
